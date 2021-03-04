11838eb8b333b414c0383f4345b409912516f462
GCLM_W10_oct <GCLM_W10_oct@DESKTOP-39EJKB9.localdomain>
2021-03-04 20:20:34 +0100

    Primeiro commit

---
+++ src/main/java/laura/codet04e01/NewMain.java
@@ -0,0 +1,8 @@
+
+package laura.codet04e01;
+
+
+public class NewMain {
+public static void main(String[] args) {
+System.out.println(Sumador.sumar (15f,4f));
+}}
---
+++ src/main/java/laura/codet04e01/Sumador.java
@@ -0,0 +1,7 @@
+
+package laura.codet04e01;
+
+public class Sumador {
+public static float sumar (float a, float b ) {
+return a + b;
+}}
